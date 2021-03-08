7d2261fa30a9ead0da1b9610fecb3b6f4ca3e2c6
JonShort <JonShort@users.noreply.github.com>
2021-03-08 16:59:05 +0000

    rustfmt on utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,117 +3,117 @@ use std::io::Write;
 
 #[derive(PartialEq)]
 pub enum ParseStatus {
-  Active,
-  Inactive,
-  Ignore,
+    Active,
+    Inactive,
+    Ignore,
 }
 
 #[derive(PartialEq, Debug)]
 pub struct Config {
-  pub keyword: String,
+    pub keyword: String,
 }
 
 impl Config {
-  pub fn new<T>(mut args: T) -> Result<Config, &'static str>
-  where
-    T: Iterator<Item = String>,
-  {
-    // ignore first arg (program name)
-    args.next();
+    pub fn new<T>(mut args: T) -> Result<Config, &'static str>
+    where
+        T: Iterator<Item = String>,
+    {
+        // ignore first arg (program name)
+        args.next();
 
-    let keyword = match args.next() {
-      Some(word) => word,
-      None => return Err("Keyword missing"),
-    };
+        let keyword = match args.next() {
+            Some(word) => word,
+            None => return Err("Keyword missing"),
+        };
 
-    Ok(Config { keyword })
-  }
+        Ok(Config { keyword })
+    }
 }
 
 #[derive(PartialEq, Debug)]
 pub struct EnvContents {
-  pub contents: String,
+    pub contents: String,
 }
 
 impl EnvContents {
-  pub fn new(contents: String) -> EnvContents {
-    EnvContents { contents }
-  }
+    pub fn new(contents: String) -> EnvContents {
+        EnvContents { contents }
+    }
 }
 
 pub fn read_env_file() -> Result<EnvContents, &'static str> {
-  let contents = match fs::read_to_string(".env") {
-    Ok(w) => w,
-    Err(_) => return Err("Unable to read .env file"),
-  };
-  Ok(EnvContents { contents })
+    let contents = match fs::read_to_string(".env") {
+        Ok(w) => w,
+        Err(_) => return Err("Unable to read .env file"),
+    };
+    Ok(EnvContents { contents })
 }
 
 pub fn write_to_file(env: EnvContents) -> Result<(), String> {
-  let mut file = match fs::File::create(".env") {
-    Ok(f) => f,
-    Err(e) => return Err(format!("Unable to write .env file - {}", e)),
-  };
+    let mut file = match fs::File::create(".env") {
+        Ok(f) => f,
+        Err(e) => return Err(format!("Unable to write .env file - {}", e)),
+    };
 
-  match file.write_all(env.contents.as_bytes()) {
-    Ok(_) => return Ok(()),
-    Err(e) => return Err(format!("Unable to write .env file - {}", e)),
-  };
+    match file.write_all(env.contents.as_bytes()) {
+        Ok(_) => return Ok(()),
+        Err(e) => return Err(format!("Unable to write .env file - {}", e)),
+    };
 }
 
 #[cfg(test)]
 mod config_tests {
-  use super::*;
-  #[test]
-  fn err_on_missing_keyword() {
-    let args = vec![String::from("")];
-    let result = Config::new(args.into_iter());
-    assert_eq!(result, Err("Keyword missing"))
-  }
-  #[test]
-  fn returns_populated_config() {
-    let args = vec![String::from(""), String::from("testing")];
-    let result = Config::new(args.into_iter()).unwrap();
-    assert_eq!(
-      result,
-      Config {
-        keyword: String::from("testing")
-      }
-    )
-  }
+    use super::*;
+    #[test]
+    fn err_on_missing_keyword() {
+        let args = vec![String::from("")];
+        let result = Config::new(args.into_iter());
+        assert_eq!(result, Err("Keyword missing"))
+    }
+    #[test]
+    fn returns_populated_config() {
+        let args = vec![String::from(""), String::from("testing")];
+        let result = Config::new(args.into_iter()).unwrap();
+        assert_eq!(
+            result,
+            Config {
+                keyword: String::from("testing")
+            }
+        )
+    }
 }
 
 #[cfg(test)]
 mod env_contents_tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn includes_correct_data() {
-    let e = EnvContents::new(String::from("testing"));
+    #[test]
+    fn includes_correct_data() {
+        let e = EnvContents::new(String::from("testing"));
 
-    assert_eq!(
-      e,
-      EnvContents {
-        contents: String::from("testing")
-      }
-    )
-  }
+        assert_eq!(
+            e,
+            EnvContents {
+                contents: String::from("testing")
+            }
+        )
+    }
 }
 
 #[cfg(test)]
 mod read_env_file_tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn does_not_error() {
-    let result = read_env_file();
-    assert_ne!(result, Err("Unable to read .env file"));
-  }
-  #[test]
-  fn returns_expected_content() {
-    let result = read_env_file();
-    let contents = String::from(
-      "# ++ one ++
+    #[test]
+    fn does_not_error() {
+        let result = read_env_file();
+        assert_ne!(result, Err("Unable to read .env file"));
+    }
+    #[test]
+    fn returns_expected_content() {
+        let result = read_env_file();
+        let contents = String::from(
+            "# ++ one ++
 # TEST_A=1
 # TEST_B=1
 
@@ -125,7 +125,7 @@ mod read_env_file_tests {
 TEST_A=3
 TEST_B=3
 ",
-    );
-    assert_eq!(result, Ok(EnvContents { contents }));
-  }
+        );
+        assert_eq!(result, Ok(EnvContents { contents }));
+    }
 }
