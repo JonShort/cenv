b540d4876e613bac31a9a1fd09f57d2ace0cc54e
JonShort <JonShort@users.noreply.github.com>
2021-08-01 16:37:12 +0100

    [cenv] - add keyword listing feature on invalid keyword provide

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,31 @@ use std::process;
 use cenv_core::parser;
 use cenv_core::utils;
 
+fn print_keywords(env: &utils::EnvContents) {
+    println!("Available keywords:");
+
+    for k in parser::list_available_keywords(env).into_iter() {
+        println!("- {}", k);
+    }
+}
+
 fn main() {
-    let config = utils::Config::new_from_args(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
     let env = utils::read_env_file().unwrap_or_else(|err| {
         eprintln!("Problem reading .env file: {}", err);
         process::exit(1);
     });
 
-    let new_env = match parser::parse_env(&env, &config) {
-        Ok(env) => env,
-        Err(e) => {
-            eprintln!("Problem parsing env: {}", e);
-            process::exit(1);
-        }
-    };
+    let config = utils::Config::new_from_args(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        print_keywords(&env);
+        process::exit(1);
+    });
+
+    let new_env = parser::parse_env(&env, &config).unwrap_or_else(|err| {
+        eprintln!("Problem parsing env: {}", err);
+        print_keywords(&env);
+        process::exit(1);
+    });
 
     match utils::write_env_file(&new_env) {
         Ok(_) => println!("Updated .env to {}", &config.keyword),
